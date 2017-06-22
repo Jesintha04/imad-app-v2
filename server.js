0bb5e53169ecf4832308c51b48b256f92fd9ac64
Jesintha04 <jessy.geo16@gmail.com>
2017-06-22 20:38:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/file-one', function (req,res) {
+app.get('/file-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'file-one.html'));
 });
 
